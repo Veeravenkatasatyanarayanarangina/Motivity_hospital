5437df331a029f60ae8395d55ab0cf2eccaf25e4
Veera venkata satyanarayana <satyanarayanarangina@gmail.com>
2022-11-07 12:56:39 +0530

    .

--- src/main/java/com/motivity/Hospital/MaxDoctorPatientAppApplication.java
+++ src/main/java/com/motivity/Hospital/MaxDoctorPatientAppApplication.java
@@ -3,7 +3,7 @@ package com.motivity.Hospital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.motivity.Hospital.controllers"})
 public class MaxDoctorPatientAppApplication {
 
 	public static void main(String[] args) {
